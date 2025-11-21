5811c23a7ef9db5ffd38a861b9fd138a0c636ef7
Samuel Bartz-Minlah <kjquarm@gmail.com>
2025-11-21 12:45:08 +0000

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,4 +16,27 @@ const teacher1: Teacher = {
   contract: false,
 };
 
+
 console.log(teacher1);
+
+interface Director {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience: number;
+  numberOfReports: number;
+  location: string;
+  contract: boolean;
+};
+
+const director1: Director = {
+  firstName: "Kofi",
+  lastName: "Mireku",
+  fullTimeEmployee: true,
+  yearsOfExperience: 10,
+  numberOfReports: 17,
+  location: "Kumasi",
+  contract: false,
+};
+
+console.log(director1);
