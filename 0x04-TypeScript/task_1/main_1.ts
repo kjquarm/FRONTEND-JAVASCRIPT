7e880135d9b6766b1887075097147b1cd3540762
Samuel Bartz-Minlah <kjquarm@gmail.com>
2025-11-21 12:39:53 +0000

    Add files via upload

---
+++ 0x04-TypeScript/task_1/main_1.ts
@@ -0,0 +1,19 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience: number;
+  location: string;
+  contract: boolean;
+};
+
+const teacher1: Teacher = {
+  firstName: "Akwasi",
+  lastName: "London",
+  fullTimeEmployee: true,
+  yearsOfExperience: 5,
+  location: "Accra",
+  contract: false,
+};
+
+console.log(teacher1);
